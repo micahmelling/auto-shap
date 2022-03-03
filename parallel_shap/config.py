efaca375b682921261ceddd9d9d6446ddd1984d9
micahmelling <micahmelling@gmail.com>
2022-03-03 00:13:24 -0600

    refactor and add .gitignore

---
+++ parallel_shap/config.py
@@ -0,0 +1,8 @@
+LINEAR_MODEL_NAMES = [
+    'logisticregression', 'linearregression', 'elasticnet', 'ridge', 'lasso'
+]
+
+TREE_MODEL_NAMES = [
+    'randomforest', 'extratrees', 'decisiontree', 'boost'
+]
+
--- parallel_shap/parallel_shap.py
+++ parallel_shap/parallel_shap.py
@@ -3,84 +3,12 @@ import pandas as pd
 import numpy as np
 import os
 import matplotlib.pyplot as plt
-import multiprocessing as mp
 
-from functools import partial
 from statistics import mean
 
-
-# move to helpers
-def make_directories_if_not_exists(directories_list: list):
-    """
-    Makes directories in the current working directory if they do not exist.
-
-    :param directories_list: list of directories to create
-    """
-    for directory in directories_list:
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-
-
-# move to helpers
-def run_shap_explainer(x_df: pd.DataFrame, explainer: callable, boosting_model: bool,
-                       regression_model: bool) -> np.array:
-    """
-    Runs the SHAP explainer on a dataframe of predictors (i.e. x_df).
-
-    :param x_df: x dataframe
-    :param explainer: SHAP explainer object
-    :param boosting_model: Boolean of whether or not the explainer is for a boosting model
-    :param regression_model: Boolean of whether or not the explainer is for a regression model; if False, it is assumed
-    we are dealing with a classification model
-    """
-    if boosting_model or regression_model:
-        return explainer.shap_values(x_df)
-    else:
-        return explainer.shap_values(x_df)[1]
-
-
-# move to helpers
-def set_n_jobs(n_jobs: int, x_df: pd.DataFrame) -> int:
-    """
-    Sets the number of n_jobs, processes to run in parallel. If n_jobs is not specified, the max number of CPUs is
-    used. If n_jobs is set to a higher amount than the number of observations in x_df, n_jobs is rebalanced to match
-    the length of x_df.
-
-    :param n_jobs: number of jobs to run in parallel
-    :param x_df: x dataframe
-    :return: number of jobs to run in parallel, using the above logic
-    """
-    if not n_jobs:
-        n_jobs = mp.cpu_count()
-    if n_jobs > len(x_df):
-        n_jobs = len(x_df)
-    return n_jobs
-
-
-# move to helpers
-def run_parallel_shap_explainer(x_df: pd.DataFrame, explainer: callable, boosting_model: bool, regression_model: bool,
-                                n_jobs: int = None) -> np.array:
-    """
-    Splits x_df into evenly-split partitions based on the n_jobs parameter. If n_jobs is not specified, the max number
-    of CPUs is used. If n_jobs is set to a higher amount than the number of observations in x_df, n_jobs is rebalanced
-    to match the length of x_df.Then, the SHAP explainer object is run in parallel on each subset of x_df. The results
-    are combined into a single object.
-
-    :param x_df: x dataframe
-    :param explainer: SHAP explainer object
-    :param boosting_model: Boolean of whether or not the explainer is for a boosting model
-    :param regression_model: Boolean of whether or not the explainer is for a regression model; if False, it is assumed
-    we are dealing with a classification model
-    :param n_jobs: number of cores to use when processing
-    """
-    n_jobs = set_n_jobs(n_jobs, x_df)
-    array_split = np.array_split(x_df, n_jobs)
-    shap_fn = partial(run_shap_explainer, explainer=explainer, boosting_model=boosting_model,
-                      regression_model=regression_model)
-    with mp.Pool(processes=n_jobs) as pool:
-        result = pool.map(shap_fn, array_split)
-    result = np.concatenate(result)
-    return result
+from parallel_shap.utilities import make_directories_if_not_exists, run_parallel_shap_explainer, make_shap_df, \
+    determine_if_name_in_object, determine_if_any_name_in_object, determine_if_regression_model, save_expected_value
+from parallel_shap.config import LINEAR_MODEL_NAMES, TREE_MODEL_NAMES
 
 
 def get_shap_expected_value(explainer: callable, boosting_model: bool) -> float:
@@ -114,18 +42,6 @@ def generate_shap_global_values(shap_values: np.array, x_df: pd.DataFrame) -> pd
     df = pd.DataFrame(list(zip(x_df.columns, shap_values)), columns=['feature', 'shap_value'])
     df.sort_values(by=['shap_value'], ascending=False, inplace=True)
     return df
-
-
-# move to helpers
-def make_shap_df(shap_values: np.array, x_df: pd.DataFrame) -> pd.DataFrame:
-    """
-    Converts a numpy array of SHAP values into a dataframe, using the column names from x_df.
-
-    :param shap_values: array of SHAP values
-    :param x_df: x dataframe
-    :return: dataframe of SHAP values
-    """
-    return pd.DataFrame(shap_values, columns=list(x_df))
 
 
 def produce_shap_output_with_kernel_explainer(model: callable, x_df: pd.DataFrame, boosting_model: bool,
@@ -280,7 +196,7 @@ def produce_raw_shap_values(model: callable, x_df: pd.DataFrame, use_kernel: boo
         return produce_shap_output_for_calibrated_classifier(model, x_df, boosting_model, linear_model)
 
 
-def generate_shap_summary_plot(shap_values: np.array, x_df: pd.DataFrame, plot_type: str, save_path : str,
+def generate_shap_summary_plot(shap_values: np.array, x_df: pd.DataFrame, plot_type: str, save_path: str,
                                file_prefix: str = None):
     """
     Generates a summary plot of SHAP values and saves it locally.
@@ -289,7 +205,7 @@ def generate_shap_summary_plot(shap_values: np.array, x_df: pd.DataFrame, plot_t
     :param x_df: x dataframe
     :param plot_type: the type of plot we want to generate; generally, either 'dot' or 'bar'
     :param save_path: path in which to save the plot
-    :param file_prefix: prefix to add to the file name.
+    :param file_prefix: prefix to add to the file name
     """
     shap.summary_plot(shap_values, x_df, plot_type=plot_type, show=False)
     if not file_prefix:
@@ -298,52 +214,6 @@ def generate_shap_summary_plot(shap_values: np.array, x_df: pd.DataFrame, plot_t
         file_name = f'{file_prefix}_shap_values_{plot_type}.png'
     plt.savefig(os.path.join(save_path, file_name), bbox_inches='tight')
     plt.clf()
-
-
-# move to helpers
-def determine_if_name_in_object(name: str, py_object: object) -> bool:
-    """
-    Determine if a name is in a Python object.
-
-    :param name: name to search for in py_object
-    :param py_object: Python object
-    """
-    object_str = str((type(py_object))).lower()
-    if name in object_str:
-        return True
-    else:
-        return False
-
-
-# move to helpers
-def determine_if_any_name_in_object(names_list: list, py_object: object) -> bool:
-    """
-    Determines if any name in names_list is also in the name of py_object, an arbitrary Python object.
-
-    :param names_list: list of names to search fow
-    :param py_object: an arbitrary Python object
-    :return: boolean classification of if a match was found
-    """
-    for name in names_list:
-        result = determine_if_name_in_object(name, py_object)
-        if result:
-            return result
-
-
-# move to helpers
-def determine_if_regression_model(model: callable) -> bool:
-    """
-    Determines if model is a regression model.
-
-    :param model: fitted model
-    :return: Boolean of whether or not the model is a regression
-    """
-    regression = determine_if_name_in_object('regression', model)
-    log_reg_check = determine_if_name_in_object('logistic', model)
-    if regression and not log_reg_check:
-        return True
-    else:
-        return False
 
 
 def generate_shap_values(model: callable, x_df: pd.DataFrame, linear_model: bool = None, tree_model: bool = None,
@@ -373,10 +243,9 @@ def generate_shap_values(model: callable, x_df: pd.DataFrame, linear_model: bool
     SHAP explainer, and a dataframe of global SHAP values
     """
     if not linear_model:
-        linear_model = determine_if_any_name_in_object(['logisticregression', 'linearregression', 'elasticnet', 'ridge',
-                                                        'lasso'], model)
+        linear_model = determine_if_any_name_in_object(LINEAR_MODEL_NAMES, model)
     if not tree_model:
-        tree_model = determine_if_any_name_in_object(['randomforest', 'extratrees', 'decisiontree', 'boost'], model)
+        tree_model = determine_if_any_name_in_object(TREE_MODEL_NAMES, model)
     if not boosting_model:
         boosting_model = determine_if_name_in_object('boost', model)
     if not calibrated_model:
@@ -389,20 +258,11 @@ def generate_shap_values(model: callable, x_df: pd.DataFrame, linear_model: bool
     return shap_values_df, shap_expected_value, global_shap_df
 
 
-# move to helpers
-def save_expected_value(expected_value: float, save_path: os.path):
-    """
-    Saves the SHAP expected value to a txt file.
-
-    :param expected_value: expected value
-    :param save_path: path in which to save the file
-    """
-    with open(os.path.join(save_path, 'shap_expected_value.txt'), 'w') as f:
-        f.write(str(expected_value))
-
-
-def produce_shap_values_and_summary_plots(model, x_df, model_uid, save_path, linear_model, tree_model, boosting_model,
-                                          calibrated_model, regression_model, use_kernel):
+def produce_shap_values_and_summary_plots(model: callable, x_df: pd.DataFrame, save_path: str,
+                                          linear_model: bool = None,  tree_model: bool = None,
+                                          boosting_model: bool = None, calibrated_model: bool = None,
+                                          regression_model: bool = None, use_kernel: bool = None,
+                                          file_prefix: str = None):
     """
     Produces SHAP values for x_df and writes associated diagnostics locally. The following output is saved in
     save_path in appropriate subdirectories: SHAP values for every row in x_df, global SHAP values for every feature
@@ -417,6 +277,19 @@ def produce_shap_values_and_summary_plots(model, x_df, model_uid, save_path, lin
     can specify if they want to use Kernel SHAP, which is appropriate in certain cases but is computationally expensive.
     If using Kernel SHAP, it's recommended to use a sample of x_df.
 
+    :param model: fitted model
+    :param x_df: x dataframe
+    :param save_path: path in which to save the plot
+    :param linear_model: Boolean of whether model is a linear model, which would employ Linear SHAP
+    :param tree_model: Boolean of whether model is a tree-based model, which would employ Tree SHAP
+    :param boosting_model: Boolean of whether or not the explainer is for a boosting model
+    :param calibrated_model: Boolean of whether or not the model is a CalibratedClassifierCV
+    :param regression_model: Boolean of whether or not this is a regression model; if not, it's assumed this is a
+    classification model
+    :param use_kernel: Boolean of whether or not to use Kernel SHAP
+    :return: tuple with three components: dataframe of SHAP values for every row in x_df, the expected value from the
+    SHAP explainer, and a dataframe of global SHAP values
+    :param file_prefix: prefix to add to the file name
     """
     save_file_path = os.path.join(save_path, 'files')
     save_plots_path = os.path.join(save_path, 'plots')
@@ -433,14 +306,10 @@ def produce_shap_values_and_summary_plots(model, x_df, model_uid, save_path, lin
     shap_values_df.to_csv(os.path.join(save_file_path, 'local_shap_values.csv'), index=False)
     global_shap_df.to_csv(os.path.join(save_file_path, 'global_shap_values.csv'), index=False)
     save_expected_value(shap_expected_value, save_file_path)
-    generate_shap_summary_plot(shap_values_df, x_df, model_uid, 'dot')
-    generate_shap_summary_plot(shap_values_df, x_df, model_uid, 'bar')
+    generate_shap_summary_plot(shap_values_df, x_df, 'bar', save_path, file_prefix)
+    generate_shap_summary_plot(shap_values_df, x_df, 'dot', save_path, file_prefix)
 
 
-# TODO: helpers file
-# TODO: add config file
-# TODO: error handling where needed
-# TODO: use kwargs when appropriate
 # TODO: build out repo
 # TODO: tests
 # TODO: documentation (readme)
